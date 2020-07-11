751e708b87b799f04c4503afe3ade9f7e94b17a5
3imed-jaberi <imed_jebari@hotmail.fr>
2020-07-11 02:13:20 +0200

    update src code --linter 🚀 ..

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ const uuid = require('uuid');
  * Expose `xRequestId()`.
  */
 
-module.exports = xRequestId
+module.exports = xRequestId;
 
 /**
  * Generates an unique Request ID for every incoming HTTP request.
  * This unique ID is then passed to your application as an HTTP header called
  * `X-Request-Id`.
- * 
- * 
+ *
+ *
  * @api public
  * */
 
-function xRequestId (options) {
+function xRequestId(options) {
   options = options || {};
 
   // TODO: add custom id generator.
@@ -41,20 +41,22 @@ function xRequestId (options) {
   options.uuidOpts = options.uuidOpts || {};
   options.uuidBuffer = options.uuidBuffer || undefined;
   options.uuidOffset = options.uuidOffset || 0;
+  // eslint-disable-next-line no-implicit-coercion
   options.noHyphen = !!options.noHyphen;
 
   options.setHeader =
+    // eslint-disable-next-line no-implicit-coercion
     options.setHeader === undefined || !!options.setHeader;
   options.headerName = options.headerName || 'X-Request-Id';
   options.attributeName = options.attributeName || 'id';
 
-  return function (ctx, next) {
-    ctx.request[options.attributeName] = 
+  return function(ctx, next) {
+    ctx.request[options.attributeName] =
       ctx.id ||
       ctx.requestId ||
       ctx.xRequestId ||
       ctx.query[options.headerName.toLowerCase()] ||
-      ctx.get(options.headerName.toLowerCase()) || 
+      ctx.get(options.headerName.toLowerCase()) ||
       // ctx.request.headers[options.headerName.toLowerCase()]
       // ctx.request.get(options.headerName.toLowerCase())
       uuidGenerator(options);
@@ -66,23 +68,24 @@ function xRequestId (options) {
 
     return next();
   };
-};
+}
 
 /**
  * Default function to generate an unique id.
- * 
+ *
  * @options {Object}: same as uuid options object
- *                    with one plus option to 
+ *                    with one plus option to
  *                    specificate of the uuid version.
  * @api private
  * */
 
 function uuidGenerator(options) {
   const id = uuid[options.uuidVersion](
-      options.uuidOpts,
-      options.uuidBuffer,
-      options.uuidOffset
-    );
-  
+    options.uuidOpts,
+    options.uuidBuffer,
+    options.uuidOffset
+  );
+
+  // eslint-disable-next-line no-useless-escape
   return options.noHyphen ? id.replace(/\-/g, '') : id;
 }
